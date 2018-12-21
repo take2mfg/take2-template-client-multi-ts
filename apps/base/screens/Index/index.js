0032bcc1af31cde148fb179a1d762f9a29608ca2
Ty Barho <t.barho@gmail.com>
2018-12-21 14:45:56 -0600

    add in graph example

--- apps/base/screens/Index/index.js
+++ apps/base/screens/Index/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import get from 'lodash/get';
+import map from 'lodash/map';
 import { compose } from 'redux';
 import { withNamespaces } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import { Query } from 'react-apollo';
+
+import { SHOPS } from '../../graphql/queries';
 
 const _Index = ({ t }) => {
   return (
@@ -10,6 +15,32 @@ const _Index = ({ t }) => {
         {t('Hello World')}
       </p>
       <Link to="/signin">{t('Sign In')}</Link>
+      <Query query={SHOPS}>
+        {({ data, loading, error }) => {
+
+          if (loading) {
+            return (
+              <p>Loading...</p>
+            );
+          }
+
+          if (error) {
+            return (
+              <p>Try opening <a href="https://codesandbox.io/s/1x5noor93" target="_blank">https://codesandbox.io/s/1x5noor93</a> to restart the server</p>
+            );
+          }
+
+          const shops = map(get(data, 'shops'), ({ id, name }) => (
+            <li key={id}>{name}</li>
+          ));
+
+          return (
+            <ul>
+              {shops}
+            </ul>
+          );
+        }}
+      </Query>
     </div>
   );
 };
