28e8cb89e68a777899a1455f638ad74a56185c06
Ty Barho <t.barho@gmail.com>
2019-01-14 10:49:40 -0600

    some fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 module.exports = function(env) {
   console.log(`Building webpack for (${env}) environment`);
 
+  /* apps */
   const BaseApp = (env == 'prod') ? require('./apps/___base___/webpack/prod') : require('./apps/___base___/webpack/dev');
+  /* appsstop */
 
   return [
     BaseApp,
