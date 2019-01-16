931e9390d5caf3c2a90cd7646dfdab439280af22
Ty Barho <t.barho@gmail.com>
2019-01-16 15:27:41 -0600

    better base

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,5 +1,4 @@
-import 'storybook-addon-jsx/register';
-import '@storybook/addon-notes/register';
+import '@storybook/addon-options/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-a11y/register';
 import '@storybook/addon-viewport/register';
--- apps/___base___/stories/Button.js
+++ apps/___base___/stories/Button.js
@@ -3,19 +3,17 @@ import { storiesOf } from '@storybook/react';
 import { Button } from '@storybook/react/demo';
 import { action } from '@storybook/addon-actions';
 
-storiesOf('Button', module)
+storiesOf('BaseApp|Button', module)
   .addDecorator(story => <div style={{ textAlign: 'center' }}>{story()}</div>)
-  .addWithJSX('with text', () => (
+  .add('with text', () => (
     <Button onClick={action('button-click')}>Hello Button</Button>
   ));
 
 
-storiesOf('Button/Emoji', module)
-  .addWithJSX('with some emoji', () => (
+storiesOf('BaseApp|Button/Emoji', module)
+  .add('with some emoji', () => (
     <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
-  ), {
-    notes: { markdown: `
-### An emoji button
-You know you like it.
-    ` },
-  });
+  ), { info: { text: `
+    ### Additional Info
+    You can use markdown to add additional info to a story
+  `} });
