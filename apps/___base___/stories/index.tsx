4f893ff3024fa47ca7c5712674a2c14646fb1348
colinphanna <github@colinphanna.com>
2019-01-25 08:40:07 -0600

    add more types

--- apps/___base___/stories/index.tsx
+++ apps/___base___/stories/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Button } from '@storybook/react/demo';
 import { action } from '@storybook/addon-actions';
