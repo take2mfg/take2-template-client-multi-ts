0dbb0fb06328c123f73b3dcba16e13c37938aa87
colinphanna <github@colinphanna.com>
2019-01-28 17:14:24 -0600

    route.js should allow /appName and /appName/

--- apps/___base___/route.js
+++ apps/___base___/route.js
@@ -9,11 +9,13 @@ module.exports = {
   assets: null,
   template: template(fs.readFileSync(path.resolve(__dirname, './index.template.html'))),
   route: function(router, app) {
-    router.get(`/${appName}/*`, (req, res, next) => {
+    const route = (req, res, next) => {
       const data = {
         assets: app.assets,
       };
       res.status(200).send(app.template(data));
-    });
+    }
+    router.get(`/${appName}/*`, route);
+    router.get(`/${appName}`, route);
   },
 };
