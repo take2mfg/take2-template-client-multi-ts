2f4afff1b29f8ba4142c599d80a4d02c467f2091
Ty Barho <t.barho@gmail.com>
2019-02-07 09:49:46 -0600

    dynamic storybook

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,25 @@
+const isVisualRegression = !!(process.env.VIZ_REG);
+
+const testPathIgnorePatterns = isVisualRegression ? ['node_modules'] : [
+  '/node_modules/',
+  '/stories.spec.tsx',
+  '/stories.spec.js',
+];
+
+const testRegex = isVisualRegression ? '(/__tests__/.*|(.|/)(stories.)(test|spec)).tsx?$' : '(/__tests__/.*|(.|/)(test|spec)).tsx?$';
+
 module.exports = {
   roots: ['<rootDir>/apps'],
   transform: {
     '^.+.tsx?$': 'ts-jest',
     '^.+\\.jsx?$': 'babel-jest',
   },
-  testRegex: '(/__tests__/.*|(.|/)(test|spec)).tsx?$',
+  testRegex,
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   reporters: ['jest-spec-reporter', 'jest-junit'],
   globalSetup: '<rootDir>test/globalSetup.js',
   setupTestFrameworkScriptFile: '<rootDir>test/testSetup.js',
+  testPathIgnorePatterns,
   globals: {
     'ts-jest': {
       diagnostics: false,
